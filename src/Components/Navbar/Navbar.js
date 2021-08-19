b6e1c672c73d457946d78fde8ad22f508e966ef1
ShasheeshPurohit <75235962+ShasheeshPurohit@users.noreply.github.com>
2021-08-19 22:20:41 +0530

    updated login field properties

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -63,8 +63,8 @@ export default function Navbar(){
     </ul>
 
     <div className="login-box" style={{visibility: loginBox?"initial":"hidden"}}>
-      <input className="login-field auth-field" placeholder="Username" onChange={(event)=>setLoginUsername(event.target.value)}/>
-      <input className="login-field auth-field" placeholder="Password" onChange={(event)=>setLoginPassword(event.target.value)}/>      
+      <input className="login-field auth-field" type="text" placeholder="Username" onChange={(event)=>setLoginUsername(event.target.value)}/>
+      <input className="login-field auth-field" type="email" placeholder="Password" onChange={(event)=>setLoginPassword(event.target.value)}/>      
       <button className="login-button auth-field" onClick={()=>{
         loginHandler (loginUsername, loginPassword)
         setLoginBox(!loginBox)}}>Login</button>
